05f46d9b01aa00821ffa43e42fb5d100b6883a31
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-29 00:12:46 +0100

    install pug

--- server.js
+++ server.js
@@ -4,3 +4,29 @@ const express = require('express'),
 	config = require('./config'),
 	app = express(),
 	googleProfile = {};
+
+passport.serializeUser( (user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser( (obj, done) => {
+	done(null, obj);
+});
+
+passport.use(new GoogleStrategy ({
+	ClientID: config.GOOGLE_CLIENT_ID,
+	clientSecret: config.GOOGLE_CLIENT_SECRET,
+	callbackURL: config.CALLBACK_URL
+}, function(accessToken, refreshToken, profile, cb) {
+		googleProfile = {
+			id: profile.id,
+			displayName: profile.displayName
+		};
+		cb(null, profile);
+	}
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
