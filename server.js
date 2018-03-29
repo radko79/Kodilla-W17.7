4382c4e709171804507295d48cb25cc0d59b1f1e
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-29 12:16:11 +0100

    final touch

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express'),
-	passport = require('passport'),
-	GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-	config = require('./config'),
-	app = express(),
-	googleProfile = {};
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+let app = express();
+let googleProfile = {};
 
 passport.serializeUser( (user, done) => {
 	done(null, user);
@@ -13,18 +13,18 @@ passport.deserializeUser( (obj, done) => {
 	done(null, obj);
 });
 
-passport.use(new GoogleStrategy ({
-	clientID: config.GOOGLE_CLIENT_ID,
-	clientSecret: config.GOOGLE_CLIENT_SECRET,
-	callbackURL: config.CALLBACK_URL
-}, 
-function(accessToken, refreshToken, profile, cb) {
-		googleProfile = {
-			id: profile.id,
-			displayName: profile.displayName
-		};
-		cb(null, profile);
-	}
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret:config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
 ));
 
 app.set('view engine', 'pug');
